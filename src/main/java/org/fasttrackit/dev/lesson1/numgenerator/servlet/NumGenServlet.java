3fbbe5fc07d8639e468b9fb9b821beb16f4534c5
Haiduku <mihai.traian.daniel@gmail.com>
2015-03-27 19:37:03 +0200

    Change theme with a dropdown the guessed number is returned from the server

--- src/main/java/org/fasttrackit/dev/lesson1/numgenerator/servlet/NumGenServlet.java
+++ src/main/java/org/fasttrackit/dev/lesson1/numgenerator/servlet/NumGenServlet.java
@@ -78,7 +78,7 @@ public class NumGenServlet extends HttpServlet {
                 boolean success = nbl.determineGuess(iGuessNumber);
                 String hint = nbl.getHint();
                 int nrGuesses = nbl.getNumGuesses();
-                jsonResponse = "{\"keySuccess\":\"" + success + "\", \"keyHint\":\"" + hint + "\", \"keyNrGuesses\":\"" + nrGuesses + "\"}";
+                jsonResponse = "{\"keySuccess\":\"" + success + "\", \"keyHint\":\"" + hint + "\", \"keyNrGuesses\":\"" + nrGuesses + "\", \"inputNumber\" :\"" +iGuessNumber +"\"}";
 
             } else {
                 jsonResponse = "{\"keyError\":\"WRONGNUMBERFORMAT\"}";
